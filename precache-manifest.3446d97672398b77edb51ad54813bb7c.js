f3c1b8695632c8baf647bc4259705411bfcb3516
Nikita K <kuznetsov.nikita@gmail.com>
2019-10-31 22:07:53 -0400

    Updates

---
+++ precache-manifest.3446d97672398b77edb51ad54813bb7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96256aaf28cb43112a096da269154d8e",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "62050f1593c4737a7e23",
+    "url": "/personal/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7f5e9ba4177cbba3f82c",
+    "url": "/personal/static/js/2.2617b79c.chunk.js"
+  },
+  {
+    "revision": "62050f1593c4737a7e23",
+    "url": "/personal/static/js/main.fe906775.chunk.js"
+  },
+  {
+    "revision": "6d10fecfe811897c032d",
+    "url": "/personal/static/js/runtime-main.ee5b5323.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/personal/static/media/logo.25bf045c.svg"
+  }
+]);
