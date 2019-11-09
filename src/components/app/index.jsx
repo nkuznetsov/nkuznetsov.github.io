3bcc9df70b30e556bda5025010059e9bdfd63716
Nikita K <kuznetsov.nikita@gmail.com>
2019-11-08 19:32:55 -0500

    home page image update

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import oyster from '../../assets/images/oyster.svg';
+import lobster from '../../assets/images/lobster.svg';
 
 function App() {
 	return (
 		<div className="app">
-			<p>Just a fluffy oyster here for now</p>
-			<img src={oyster} className="oyster" alt="oyster" />
+			<p>Just a fluffy lobster here for now</p>
+			<img src={lobster} className="lobster" alt="lobster" />
 		</div>
 	);
 }
